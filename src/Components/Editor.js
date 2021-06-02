274eeb71a9ca155df94caf3ebd9655680aa4bb4d
Pragya Jha <36370440+Pragya28@users.noreply.github.com>
2021-06-02 10:57:24 +0530

    Added comments

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,6 +1,9 @@
+// This file was created by Pragya
+
+// This is function compoment for creating Editor using CodeMirror 6
+
 import React, { useEffect, useRef } from 'react'
 import { basicSetup, EditorState, EditorView } from '@codemirror/basic-setup';
-// import { python } from '@codemirror/lang-python';
 import { java } from '@codemirror/lang-java';
 import {myTheme, myHighlightStyle } from './Style';
 import { python } from '@codemirror/lang-python';
